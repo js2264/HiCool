774d0533faea150ac39f6fe45917a696e1e00b54
js2264 <jacquesserizay@gmail.com>
2022-11-26 12:25:28 +0100

    added tests

--- tests/testthat/test-HiCool.R
+++ tests/testthat/test-HiCool.R
@@ -1,20 +1,18 @@
-test_that("Check that packages are installed", {
+test_that("Check that HiCool works", {
+    
     ## -------- Get fasta file from AnnotationHub
     # ah <- AnnotationHub::AnnotationHub()
     # twobit <- ah[['AH106801']]
     # on.exit(unlink('seq.fa'))
     # BiocIO::export(BiocIO::import(twobit), con = 'seq.fa', format = 'fasta')
-    #' 
+
     ## -------- Get fastq files from HiContactsData
     # r1 <- HiContactsData::HiContactsData(sample = 'yeast_wt', format = 'r1_fastq')
     # r2 <- HiContactsData::HiContactsData(sample = 'yeast_wt', format = 'r2_fastq')
-    #' 
+
     ## -------- Run HiCool::HiCool()
     # on.exit(unlink('./HiCool/'), add = TRUE)
     # hcf <- HiCool(r1, r2, 'seq.fa', output_folder = './HiCool/')
     # expect_s4_class(cf, 'CoolFile')
 
-    cool_path <- HiContactsData::HiContactsData(sample = 'yeast_wt', format = 'cool')
-    cf <- HiCExperiment::CoolFile(cool_path)
-    expect_s4_class(cf, 'CoolFile')
 })
---
+++ tests/testthat/test-utils.R
@@ -0,0 +1,7 @@
+test_that("Check that utils work", {
+
+    # log_path <- HiContactsData::HiContactsData(sample = 'yeast_wt', format = 'HiCool_log')
+    # expect_s3_class(getHicStats(log_path), 'list')
+    # expect_s3_class(getHgetHiCoolArgsicStats(log_path), 'list')
+    
+})
