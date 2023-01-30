7b3702704f692128f35e870188cf229147f81cf0
js2264 <jacquesserizay@gmail.com>
2023-01-30 17:29:23 +0100

    remove additional unnecessary HiCReport test

--- tests/testthat/test-HiCReport.R
+++
@@ -1,9 +0,0 @@
-test_that("Check that reports work", {
-
-    mcool_path <- HiContactsData::HiContactsData('yeast_wt', 'mcool')
-    pairs_path <- HiContactsData::HiContactsData('yeast_wt', 'pairs.gz')
-    log_path <- HiContactsData::HiContactsData(sample = 'yeast_wt', format = 'HiCool_log')
-    cf <- CoolFile(mcool_path, pairs = pairs_path, metadata = list(log = log_path))
-    expect_no_error(HiCReport(cf))
-    
-})
--- tests/testthat/test-HiCool.R
+++ tests/testthat/test-HiCool.R
@@ -5,6 +5,7 @@ test_that("Check that HiCool works with AWS S3 iGenomes", {
     r2 <- HiContactsData::HiContactsData(sample = 'yeast_wt', format = 'fastq_R2')
 
     ## -------- Run HiCool::HiCool() with AWS S3 iGenomes
+    ## HiCReport() is tested here as well, since it is executed by HiCool() as long as `build_report == TRUE`
     hcf <- HiCool(r1, r2, 'R64-1-1', output = './HiCool/')
     expect_s4_class(hcf, 'CoolFile')
 
